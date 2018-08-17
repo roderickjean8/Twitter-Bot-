9e5455e0a071e8a6b8e09aed37588cbd53866ca7
Roderick Jean <roderickjean8@gmail.com>
2018-08-17 13:02:53 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "F22vEtAAhZKlHsaiZvqo8hL7Y"
+consumer_secret = "9lSYWkIdnBJaWwtiKp04lEJ5NjLLpbfkVRbQOF4v1LYKHF2cdE"
+access_token = "1009238507674570752-r4iDJ2RKgxHbPIFgvb8ooFkxnjHIXx"
+access_token_secret = "Helcuw7niBGH7ebQxfXiuEIfKkueAJc2kH4dUBDCprAfZ"
